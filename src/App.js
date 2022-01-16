ab4ce1e43b177d9760375a5a344828fa4bcea438
BasitMaqsood <malikbasitmaqsood@gmail.com>
2022-01-17 01:58:36 +0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Concepts with Muhammad Basit Maqsood</h1>
+      <p>Please Check Branches</p>
+    </div>
+  );
+}
+
+export default App;
